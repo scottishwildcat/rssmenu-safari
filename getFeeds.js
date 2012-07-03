4995bd2b27a15d280424e037aec0294792159227
Calum Benson <scottishwildcat@gmail.com>
2012-07-03 17:57:07 +0100

    Now using associative array to store menu for each tab at load time.
    Correct menu appears for each tab, assuming extension was active when page was loaded. Need to deal with case where extension was not active when page was loaded (how?), and delete entry from array when tab is closed (how?). Also, of course, properly open the feed URL rather than just popping it up -- window.open doesn't work due to extension architecture; need to use safari.application.openBrowserWindow() instead.

---
+++ getFeeds.js
@@ -0,0 +1,44 @@
+
+safari.self.addEventListener("message", msgHandler, false); // Listen for events sent by global.html
+
+findFeedsOnPage(); // Run when any page has finished loading
+
+function msgHandler(event){
+
+	if (event.name == "getfeeds")
+		console.log("Got explicit find feeds");
+		findFeedsOnPage();
+		
+}
+
+function findFeedsOnPage(){
+
+	if (window.top === window) {
+	
+	   // The parent frame is the top-level frame, not an iframe, so we should do our stuff.
+			
+		var feeds = [];
+		var c, v;
+	
+		var docHead = document.getElementsByTagName('head')[0];
+			
+		// Store all found feeds in an array: [[name,url],[name,url],…]
+		for (i=0; i < docHead.childElementCount; i++){
+			
+			c = docHead.children[i];
+			
+			if (c.nodeName == "LINK"){
+				
+				if (c.attributes.getNamedItem("rel")!=null && 
+					c.attributes.getNamedItem("rel").value == "alternate"){
+					
+					v = c.attributes.getNamedItem("type").value;
+				
+					if (v == "application/rss+xml" || v == "application/atom+xml")
+						feeds.push([c.attributes.getNamedItem("title").value, c.attributes.getNamedItem("href").value]);
+				}
+			}
+		}
+		safari.self.tab.dispatchMessage("foundFeeds",[feeds,document.URL]);
+	}
+}
