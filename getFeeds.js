9ef39a2d7024569ff3aae2ce25bae289bdf63151
Calum Benson <scottishwildcat@gmail.com>
2012-07-04 13:08:26 +0100

    Now opens feed in default app by adding an invisible iframe to page, whose src = feed URL. (Currently adds a new frame every time you select a feed -- need to fix that.) Add proper settings for "open in Google Reader / open in default app", and "hide badge when menu opened". (Latter doesn't quite work properly -- badge always reappears when you switch back to that tab.)

--- getFeeds.js
+++ getFeeds.js
@@ -1,24 +1,39 @@
 
-//safari.self.addEventListener("message", msgHandler, false); // Listen for events sent by global.html
+safari.self.addEventListener("message", msgHandler, false); // Listen for events sent by global.html
+
+findFeedsOnPage(); // Run when any page has finished loading
 
 
-/*function msgHandler(event){
+function msgHandler(event){
 
-	if (event.name == "getfeeds")
-		console.log("Got explicit find feeds");
-		findFeedsOnPage();
+	if (event.name == "openFeedInDefaultApp"){
+		// We open feed in default app by adding an invisible iframe to the page
+		// whose src is the feed URL.
 		
+		// TODO: If we've already added an iframe, don't keep adding new ones, just
+		// change the src.
+		
+		//TODO: A nice transparent overlay saying "Opening feed in default app, may take a few seconds"
+		var ifrm = document.createElement('IFRAME');
+		ifrm.src = event.message;
+		ifrm.style.width = 0+'px';
+		ifrm.style.height = 0+'px';
+		document.body.appendChild(ifrm);
+	}	
 }
-*/
+
 
 function findFeedsOnPage(){
 
+	/* A feed is a node in the document <head> that looks like:
+	 * <link rel="alternate" type="application/rss+xml" title="RSS feed" href="http://blah.com/rss/feed.xml">
+	 * where type may also be application/atom+xml or text/xml. */
+
 	if (window.top === window) {
-	
-	   // The parent frame is the top-level frame, not an iframe, so we should do our stuff.
+	   // The parent frame is the top-level frame, not an iframe, so we can go ahead and check here.
 			
-		var feeds = [];
-		var c, v;
+		var foundFeeds = [];
+		var c, t;
 	
 		var docHead = document.getElementsByTagName('head')[0];
 			
@@ -26,17 +41,17 @@ function findFeedsOnPage(){
 		for (i=0; i < docHead.childElementCount; i++){
 			
 			c = docHead.children[i];
-			
+						
 			if (c.nodeName == "LINK"){
 				
 				if (c.attributes.getNamedItem("rel")!=null && 
 					c.attributes.getNamedItem("rel").value == "alternate"){
 					
-					v = c.attributes.getNamedItem("type").value;
+					t = c.attributes.getNamedItem("type").value;
 				
-					if (v == "application/rss+xml" || 
-						v == "application/atom+xml" ||
-						v == "text/xml")
+					if (t == "application/rss+xml" || 
+						t == "application/atom+xml" ||
+						t == "text/xml")
 						
 						var title = c.attributes.getNamedItem("title").value;
 						var href = c.attributes.getNamedItem("href").value;
@@ -46,12 +61,20 @@ function findFeedsOnPage(){
 							href = 'http://' + document.domain + href;
 						}
 						
-						feeds.push([title, href]);
+						foundFeeds.push([title, href]);
 				}
 			}
 		}
-		safari.self.tab.dispatchMessage("foundFeeds",[feeds,document.URL]);
+		safari.self.tab.dispatchMessage("foundFeeds",[foundFeeds,document.URL]);
 	}
 }
 
-findFeedsOnPage(); // Run when any page has finished loading
+function openFeedInDefaultApp(url){
+	// We do this by adding an invisible iframe to the page, whose source is the feed URL
+	var ifrm = document.createElement('IFRAME');
+	ifrm.src = url;
+	ifrm.style.width = 0+'px';
+	ifrm.style.height = 0+'px';
+	document.body.appendChild(ifrm);
+
+}
