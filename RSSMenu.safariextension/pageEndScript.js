31e8dbc8dac6e116d7f81766206c238e5ca8c594
Calum Benson <scottishwildcat@gmail.com>
2013-07-15 01:40:45 +0100

    Pull YouTube functionality into its own .js file, which is loaded dynamically only when on a YouTube page.
    Now finds and puts RSS feeds for the user's/channel's subscriptions and favourites in the unsorted section of the menu, and  feeds for their playlists in the sorted section. The sorted section now has a menu header (given to us when we requested the feeds via the YouTube API) as well as  the separator.

--- RSSMenu.safariextension/pageEndScript.js
+++ RSSMenu.safariextension/pageEndScript.js
@@ -1,6 +1,7 @@
-/** End Script for Safari RSS Feed Extension **/
-/** © 2012-13 Calum Benson                   **/
-/** Licence: None - public domain            **/      
+/** Injected Script for Safari RSS Feed Extension **/
+/** © 2012-13 Calum Benson                        **/
+/** Licence: None - public domain                 **/    
+  
 
 //"use strict";
 
@@ -14,7 +15,9 @@ if (isTopLevel()){
 	
 	var urlRegExp = new RegExp("youtube.com\/","i");
 	if (urlRegExp.test(document.URL)){
-		findYouTubePlaylistFeedsOnPage();
+		$.getScript(safari.extension.baseURI+"youtube.js")
+			.done(function(script, textStatus){findYouTubePlaylistFeedsOnPage();})
+			.fail(function(jqxhr, settings, exception) {alert("Couldn't load script");});
 	}
 }
 
@@ -278,7 +281,7 @@ function findFeedsOnPage(){
 										
 					if (href){
 						// Autodiscovered feeds are listed on menu in order found, i.e. unsorted
-						foundFeeds[href]={sort:false, title:title};
+						foundFeeds[href]={sort:false, title:title, type:'none'};
 					}
 				
 				} // type === rss+xml/atom+xml/xml
@@ -288,36 +291,4 @@ function findFeedsOnPage(){
 		} // rel==alternate
 	}
 	safari.self.tab.dispatchMessage("foundFeeds",foundFeeds);
-}
-
-function findYouTubePlaylistFeedsOnPage(){
-	// Return a list of RSS feeds for YouTube playlists on this page.
-	// Uses Google API v2, see https://developers.google.com/youtube/2.0/reference#Playlists_Feeds.
-
-	var foundFeeds = {}; // will be populated as: {url1:title1, url2: title2...}
-	
-	// Use the YouTube userId embedded in the page's <meta> information.
-	var userId = $('meta[itemprop="channelId"]').attr('content');
-
-	function gotYouTubeFeeds(data, textStatus, jqXHR){
-		var foundFeeds =[];
-		
-		if (textStatus==="success"){
-			var feeds = $('entry',data);
-			for (var i=0; i < feeds.length; i++){
-				var plTitle = $('title', $('entry',data)[i]).text();
-				var plURL = $('content', $('entry',data)[i]).attr('src') + "&max-results=50"; //Max allowed by API v2.
-				
-				// YouTube feeds are sorted alphabetically on the menu
-				foundFeeds[plURL] = {sort:true, title:plTitle};
-				clog('l',plTitle+": "+plURL);
-			}
-			safari.self.tab.dispatchMessage("foundFeeds",foundFeeds);
-		}
-	}
-	
-	if (userId !== undefined){
-		var plAPI = "https://gdata.youtube.com/feeds/api/users/"+userId+"/playlists?v=2";
-		$.get(plAPI, gotYouTubeFeeds);
-	}
 }
---
+++ RSSMenu.safariextension/youtube.js
@@ -0,0 +1,70 @@
+/** Injected Script for Safari RSS Feed Extension **/
+/** © 2012-13 Calum Benson                        **/
+/** Licence: None - public domain                 **/    
+
+
+function findYouTubePlaylistFeedsOnPage(){
+	// Return a list of RSS feeds for YouTube playlists on this page.
+	// Uses Google API v2, see https://developers.google.com/youtube/2.0/reference#Playlists_Feeds.
+
+	var foundFeeds = {}; // will be populated as: {url1:title1, url2: title2...}
+	var channelId = $('meta[itemprop="channelId"]').attr('content');
+
+	function gotYouTubePlaylists(feedType){
+
+		return function(data, textStatus, jqXHR){
+			
+			var foundFeeds =[];
+			
+			if (textStatus==="success"){
+			
+				var feeds, plTitle, plURL;
+				
+				switch (feedType){
+
+					case 'plist':
+						feeds = $('entry',data);
+						foundFeeds['menulabel'] = $('feed>title', data).text();
+						
+						for (var i=0; i < feeds.length; i++){
+							plTitle = $('title', $('entry',data)[i]).text();							
+							plURL = $('content', $('entry',data)[i]).attr('src') + "&max-results=50"; //Max allowed by API v2.
+														
+							// Playlist feeds will be sorted alphabetically on the menu
+							foundFeeds[plURL] = {sort:true, title:plTitle, type:feedType};
+						}
+						break;
+						
+					case 'favs':
+					case 'subs':
+						plURL = $('link[href*="start-index"]',data).attr('href');
+						plTitle = ($('title',data)[0]).textContent;
+						
+						// Subs feed title is returned with channelId rather than display name,
+						// so replace it first.
+						var username = $('meta[itemprop="name"]').attr("content");
+						plTitle = plTitle.replace(channelId.substring(2),username);
+
+						foundFeeds[plURL] = {sort:false, title:plTitle, type:feedType};
+						
+						break;		
+				}
+				
+				safari.self.tab.dispatchMessage("foundFeeds",foundFeeds);
+			}
+		}
+	}
+
+	
+	if (channelId !== undefined){
+		var plAPI = "https://gdata.youtube.com/feeds/api/users/"+channelId+"/playlists?v=2";
+		$.get(plAPI, gotYouTubePlaylists('plist'));
+		
+		plAPI = "https://gdata.youtube.com/feeds/api/users/"+channelId+"/favorites"
+		$.get(plAPI, gotYouTubePlaylists('favs'));
+		
+		plAPI = "https://gdata.youtube.com/feeds/api/users/"+channelId+"/newsubscriptionvideos";
+		$.get(plAPI, gotYouTubePlaylists('subs'));
+		
+	}
+}
